cee96cca87126aa15df9fd4b37a10679a8e9b204
Israel Garcia <jgarcia18@brophybroncos.org>
2020-04-03 20:40:07 -0700

    adds functionality to carousel on index page

---
+++ pyeSquared/carousel.js
@@ -0,0 +1,20 @@
+let slideIndex = 0;
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("home-carousel");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000);
+}
+
+showSlides();
